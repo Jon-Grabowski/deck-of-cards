d08ca39b2f445c03b33ec78480753d8e962db9b0
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-01-15 17:23:10 -0500

    start button updates deck and currCard states

--- src/components/HiOrLo.js
+++ src/components/HiOrLo.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { useState, useEffect, useReducer } from 'react'
+import { act } from 'react-dom/test-utils'
 
 function reducer(state, action) {
     switch (action.type) {
-        case ('deck'):
-            // console.log(action.payload)
-            return {deck : action.payload}
+        case ('shuffle'):
+            return {deck : action.payload, currCard: {}, score: 0}
+        case ('start'):
+            return {
+                ...state, 
+                deck: {
+                    remaining: action.payload.remaining, 
+                    deck_id: action.payload.deck_id
+                },
+                currCard: action.payload.cards[0]
+            }
+            // newState.deck.remaining = action.payload.remaining
+            
         default:
             return state
     }
@@ -18,10 +29,6 @@ const initialState = {
 }
 
 function HiOrLo() {
-    // const [currCard, setCurrCard] = useState({})
-    // const [deckId, setDeckId] = useState()
-    // const [score, setScore] = useState(0)
-    // const [remaining, setRemaining] = useState(0)
     const [state, dispatch] = useReducer(reducer, initialState)
 
     async function getDeck() {
@@ -29,31 +36,47 @@ function HiOrLo() {
         // DISABLED FETCHING NEW DECK DURING TESTING, INSTEAD IT SHUFFLES TESTING DECK.
         // const newDeck = await fetch ("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").then(r => r.json())
         const deck = await fetch (`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`).then(r => r.json());
-        // console.log(deck)
-        dispatch({type: 'deck', payload: deck})
-        // setRemaining(deck.remaining)
-        // setCurrCard({})
-        // setScore(0)
+        dispatch({type: 'shuffle', payload: deck})
     }
 
-    // async function drawCard() {
-    //     const newCard = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-    // }
+    async function handleStart() {
+        const deckId = "enxk4heverez" //TESTING DECK ID
+        const newCardObject = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`).then(r=>r.json())
+        assignNumVal(newCardObject.cards[0])
+        dispatch({type: 'start', payload: newCardObject})
+    }
+
+    function assignNumVal(newCard) {
+        switch (newCard.value) {
+            case 'JACK':
+                return newCard['numValue'] = 11
+            case 'QUEEN':
+                return newCard['numValue'] = 12
+            case 'KING':
+                return newCard['numValue'] = 13
+            case 'ACE':
+                return newCard['numValue'] = 14
+            default:
+                return newCard['numValue'] = parseInt(newCard.value)
+        };
+    }
 
     useEffect(() => {
         getDeck()
     }, [])
     console.log(state)
+
+
     return (
         <div id="play-area">
             <button onClick={getDeck}>Shuffle Deck</button>
-            {/* <div id='current-deck'>
+            <div>
                 <img src='https://deckofcardsapi.com/static/img/back.png' alt='back of card'/>
-                <p>Cards Remaining: {remaining}</p>
+                <p>Cards Remaining: {state.deck.remaining}</p>
             </div>
             <div>
-                {remaining > 51 ? <button>Start</button> : null}
-            </div> */}
+                {state.deck.remaining > 51 ? <button onClick={handleStart}>Start</button> : null}
+            </div>
         </div>
     )
 }
