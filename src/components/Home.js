e08d21b51b45704e58ddf5c6a3f60203f2520435
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-02-06 18:16:40 -0500

    home page css file and some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './home.css'
 
 function Home() {
 
@@ -8,9 +9,13 @@ function Home() {
             <div id='home-header'>
                 <h1>This is the Home Header</h1>
             </div>
-            <div>
-                <Link to='/hilo'><button>Hi/Lo Game</button></Link>
-                
+            <div id='home-body-container'>
+                <div id='card-games-container'>
+                    <Link to='/hilo'><button>Hi/Lo Game</button></Link>
+                </div>
+                <div id='other-games-container'>
+
+                </div>
             </div>
         </div>
     )
